285c1395ef0947360127255f4105a54126dbea30
Ranganath <ranganathcv123@gmail.com>
2026-02-27 19:21:10 +0530

    simple problem on dic

--- Read_and_handle_file_task1.py
+++ Read_and_handle_file_task1.py
@@ -2,9 +2,10 @@ try:
     file_handler=open('sample1.txt','rt')
     line_1=file_handler.readline()
     line_2=file_handler.readline()
-    print("Reding file content")
-    print(f'line 1 :{line_1}')
-    print(f'line 2 : {line_2}')
+    file_handler.close()
+    print("Reding file content\n")
+    print(f'line 1 :{line_1}\n')
+    print(f'line 2 : {line_2}\n')
 
 except FileNotFoundError as file_error:
     print(file_error)
